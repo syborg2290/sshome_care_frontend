8f2fa4ee037ca89a746120148ab1e38c0b0a7398
janith kavishka <janithdelpagodage@gmail.com>
2020-10-13 21:29:24 +0530

    modify login page some styles

--- src/main/login/Login.js
+++ src/main/login/Login.js
@@ -8,12 +8,11 @@ import {
   TextField,
 } from "@material-ui/core";
 import { withRouter } from "react-router-dom";
-import LockOpenIcon from '@material-ui/icons/LockOpen';
+import LockOpenIcon from "@material-ui/icons/LockOpen";
 
 // styles
 import useStyles from "./styles";
-import  "./Login.css";
-
+import "./Login.css";
 
 // context
 import { useUserDispatch, loginUser } from "../../context/UserContext";
@@ -34,79 +33,82 @@ function Login(props) {
     <Grid container className="container">
       <div className="formContainer">
         <div className="form">
-
-            <Tab label="Login" className="lbl_Login" />
-      
-
-            <React.Fragment>
-              <Typography variant="h3" className="greeting">
-                S S HOME CARE CITY
+          <Tab label="Login" className="lbl_Login" />
+          <React.Fragment>
+            <Typography variant="h3" className="greeting">
+              S S HOME CARE CITY
+            </Typography>
+            <div className={classes.formDividerContainer}>
+              <div className={classes.formDivider} />
+              <Typography className={classes.formDividerWord}>
+                <LockOpenIcon className="lock_Icon" />
               </Typography>
-              <div className={classes.formDividerContainer}>
-                <div className={classes.formDivider} />
-                <Typography className={classes.formDividerWord}>
-                  <LockOpenIcon className="lock_Icon" />
-                </Typography>
-                <div className={classes.formDivider} />
-              </div>
-              <TextField
-                id="email"
-                InputProps={{
-                  classes: {
-                    underline: classes.textFieldUnderline,
-                    input: classes.textField,
-                  },
-                }}
-                value={loginValue}
-                onChange={(e) => setLoginValue(e.target.value)}
-                margin="normal"
-                placeholder="Email Adress"
-                type="email"
-                fullWidth
-              />
-              <TextField
-                id="password"
-                InputProps={{
-                  classes: {
-                    underline: classes.textFieldUnderline,
-                    input: classes.textField,
-                  },
-                }}
-                value={passwordValue}
-                onChange={(e) => setPasswordValue(e.target.value)}
-                margin="normal"
-                placeholder="Password"
-                type="password"
-                fullWidth
-              />
-              <div className={classes.formButtons}>
-                {isLoading ? (
-                  <CircularProgress size={26} className="loginLoader" />
-                ) : (
-                  <Button
-                    className="btn_Login"
-                    disabled={
-                      loginValue.length === 0 || passwordValue.length === 0
-                    }
-                    onClick={() =>
-                      loginUser(
-                        userDispatch,
-                        loginValue,
-                        passwordValue,
-                        props.history,
-                        setIsLoading,
-                        setError
-                      )
-                    }
-                    variant="contained"
-                    color="primary"
-                    size="large"
-                  >
-                    Login
-                  </Button>
-                )}
-              </div>
-            </React.Fragment>
+              <div className={classes.formDivider} />
+            </div>
+            <TextField
+              id="name"
+              className="txt_login"
+              // InputProps={{
+              //   classes: {
+              //     underline: classes.textFieldUnderline,
+              //     input: classes.textField,
+              //   },
+              // }}
+              value={loginValue}
+              onChange={(e) => setLoginValue(e.target.value)}
+              // margin="normal"
+              placeholder="User Name"
+              type="name"
+              label="User Name"
+              variant="outlined"
+              fullWidth
+            />
+            <TextField
+              id="password"
+              className="txt_login"
+              // InputProps={{
+              //   classes: {
+              //     underline: classes.textFieldUnderline,
+              //     input: classes.textField,
+              //   },
+              // }}
+              value={passwordValue}
+              onChange={(e) => setPasswordValue(e.target.value)}
+              margin="normal"
+              placeholder="Password"
+              type="password"
+              label="Password"
+              variant="outlined"
+              fullWidth
+            />
+            <div className={classes.formButtons}>
+              {isLoading ? (
+                <CircularProgress size={26} className="loginLoader" />
+              ) : (
+                <Button
+                  className="btn_Login"
+                  disabled={
+                    loginValue.length === 0 || passwordValue.length === 0
+                  }
+                  onClick={() =>
+                    loginUser(
+                      userDispatch,
+                      loginValue,
+                      passwordValue,
+                      props.history,
+                      setIsLoading,
+                      setError
+                    )
+                  }
+                  variant="contained"
+                  color="primary"
+                  size="large"
+                >
+                  Login
+                </Button>
+              )}
+            </div>
+          </React.Fragment>
         </div>
       </div>
     </Grid>
--- src/main/login/styles.js
+++ src/main/login/styles.js
@@ -1,48 +1,47 @@
 import { makeStyles } from "@material-ui/styles";
 
-export default makeStyles(theme => ({
-  container: {
-    height: "100vh",
-    width: "100vw",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    position: "absolute",
-    top: 0,
-    left: 0,
-  },
-  logotypeContainer: {
-    backgroundColor: theme.palette.primary.main,
-    width: "60%",
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    [theme.breakpoints.down("md")]: {
-      width: "50%",
-    },
-    [theme.breakpoints.down("md")]: {
-      display: "none",
-    },
-  },
-  logotypeImage: {
-    width: 165,
-    marginBottom: theme.spacing(4),
-  },
-  logotypeText: {
-    color: "white",
-    fontWeight: 500,
-    fontSize: 84,
-    [theme.breakpoints.down("md")]: {
-      fontSize: 48,
-    },
-  },
+export default makeStyles((theme) => ({
+  // container: {
+  //   height: "100vh",
+  //   width: "100vw",
+  //   display: "flex",
+  //   justifyContent: "center",
+  //   alignItems: "center",
+  //   position: "absolute",
+  //   top: 0,
+  //   left: 0,
+  // },
+  // logotypeContainer: {
+  //   backgroundColor: theme.palette.primary.main,
+  //   width: "60%",
+  //   height: "100%",
+  //   display: "flex",
+  //   flexDirection: "column",
+  //   justifyContent: "center",
+  //   alignItems: "center",
+  //   [theme.breakpoints.down("md")]: {
+  //     width: "50%",
+  //   },
+  //   [theme.breakpoints.down("md")]: {
+  //     display: "none",
+  //   },
+  // },
+  // logotypeImage: {
+  //   width: 165,
+  //   marginBottom: theme.spacing(4),
+  // },
+  // logotypeText: {
+  //   color: "white",
+  //   fontWeight: 500,
+  //   fontSize: 84,
+  //   [theme.breakpoints.down("md")]: {
+  //     fontSize: 48,
+  //   },
+  // },
 
   form: {
     width: 320,
   },
-
 
   formDividerContainer: {
     marginTop: theme.spacing(4),
@@ -55,8 +54,6 @@ export default makeStyles(theme => ({
     paddingRight: theme.spacing(2),
   },
 
-
-  
   formDivider: {
     flexGrow: 1,
     height: 1,
@@ -84,7 +81,4 @@ export default makeStyles(theme => ({
     justifyContent: "space-between",
     alignItems: "center",
   },
-
-
-
 }));

5ea4f62f69326ab8077b2f4d3dc2b21edf2ee994
janith kavishka <janithdelpagodage@gmail.com>
2020-10-20 16:54:16 +0530

    print invoice changing pending

--- src/main/emp_showroom/pages_showroom/invoice/make_invoice/printInvoice/Print_invoice.js
+++ src/main/emp_showroom/pages_showroom/invoice/make_invoice/printInvoice/Print_invoice.js
@@ -141,6 +141,131 @@ class PrintInvoiceClass extends React.Component {
             </form>
           </div>
         </Container>
+
+        <br />
+
+        <Container component="main" className="mainPrint_container">
+          <Typography className="company_title" variant="h5" gutterBottom>
+            SS HOME CARE CITY
+          </Typography>
+
+          <Typography className="company_sub__title" variant="h5" gutterBottom>
+            Dealers In All Kind of Electtic & Electronic Items
+            <hr />
+          </Typography>
+
+          <div className="paper">
+            <form className="form" noValidate>
+              <Row>
+                <Col span={4}>Invoice No:</Col>
+                <Col className="invo_number" span={20}>
+                  0138900
+                </Col>
+                <Col span={24}>
+                  <br />
+                  <br />
+                </Col>
+                <Col span={2}>Tel.</Col>
+                <Col span={16}>
+                  0917901603
+                  <br />
+                  0715651711
+                </Col>
+                <Col span={5}>
+                  Mapalagama Rd.
+                  <br />
+                  <span className="lbl_address">THANABADDEGAMA.</span>
+                </Col>
+
+                <Col span={24}>
+                  <br />
+                </Col>
+                <Col span={18}></Col>
+                <Col span={6}>Date............................</Col>
+                <Col span={24}>
+                  <hr />
+                  <br />
+                </Col>
+                <Col span={24}>
+                  <TableContainer className="tbl_Containers" component={Paper}>
+                    <Table className="tables" aria-label="spanning table">
+                      <TableHead>
+                        <TableRow>
+                          <TableCell className="tbl_Cell">Item</TableCell>
+                          <TableCell className="tbl_Cell" colSpan={1}>
+                            Qty
+                          </TableCell>
+
+                          <TableCell
+                            className="tbl_Cell"
+                            align="right"
+                            colSpan={1}
+                          >
+                            DP(LKR)
+                          </TableCell>
+
+                          <TableCell className="tbl_Cell" align="right">
+                            Discount(LKR)
+                          </TableCell>
+                          <TableCell className="tbl_Cell" align="right">
+                            Sum(LKR)
+                          </TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        <TableRow>
+                          <TableCell>Cooker</TableCell>
+                          <TableCell align="right">123</TableCell>
+                          <TableCell align="right">123</TableCell>
+                          <TableCell align="right">123</TableCell>
+                          <TableCell align="right">50000.00</TableCell>
+                        </TableRow>
+
+                        <TableRow>
+                          <TableCell rowSpan={4} />
+                          <TableCell align="right" colSpan={3}>
+                            Subtotal
+                          </TableCell>
+                          <TableCell align="right" colSpan={2}>
+                            1200.00
+                          </TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell align="right" colSpan={3}>
+                            Discount
+                          </TableCell>
+                          <TableCell className="cel" align="right" colSpan={2}>
+                            123
+                          </TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell
+                            className="cell_total"
+                            align="right"
+                            colSpan={3}
+                          >
+                            Total
+                          </TableCell>
+                          <TableCell align="right" colSpan={2}>
+                            55000.00
+                          </TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                </Col>
+              </Row>
+              <br />
+              <Row className="row_signature">
+                <Col className="lbl_signature" span={4}>
+                  ...............................
+                  <br />
+                  <span className="txt_signature"> Signature</span>
+                </Col>
+              </Row>
+            </form>
+          </div>
+        </Container>
       </div>
     );
   }

eb568eb2f096ee0f6afff43c5ff4c995c0a01e83
janith kavishka <janithdelpagodage@gmail.com>
2020-10-23 13:11:03 +0530

    modify arreas table and model

--- src/main/emp_showroom/pages_showroom/arreas/Areas.js
+++ src/main/emp_showroom/pages_showroom/arreas/Areas.js
@@ -1,7 +1,166 @@
-import React from "react";
+import React, { useState } from "react";
+import MUIDataTable from "mui-datatables";
+import { Grid, Button } from "@material-ui/core";
+import { Spin, Modal } from "antd";
+// eslint-disable-next-line
+import CurrencyFormat from "react-currency-format";
 
 import "./Arreas.css";
 
+// icons
+
 export default function Areas() {
-  return <div className="ff">ds</div>;
+  // eslint-disable-next-line
+  const [isLoading, setIsLoading] = useState(true);
+
+  // eslint-disable-next-line
+  const [currentIndx, setCurrentIndx] = useState(0);
+  const [arreasUpdate, setArreasUpdate] = useState(false); //  table models
+  //START pay And Go Columns
+  const arreasTableColomns = [
+    {
+      name: "InvoiceNo",
+      options: {
+        filter: true,
+        setCellHeaderProps: (value) => ({
+          style: { fontSize: "15px", color: "black", fontWeight: "600" },
+        }),
+      },
+    },
+    {
+      name: "Delayed",
+      options: {
+        filter: true,
+        setCellHeaderProps: (value) => ({
+          style: { fontSize: "15px", color: "black", fontWeight: "600" },
+        }),
+      },
+    },
+    {
+      name: "reason",
+      options: {
+        filter: true,
+        setCellHeaderProps: (value) => ({
+          style: { fontSize: "15px", color: "black", fontWeight: "600" },
+        }),
+      },
+    },
+    {
+      name: "test",
+      options: {
+        filter: false,
+        setCellHeaderProps: (value) => ({
+          style: { fontSize: "15px", color: "black", fontWeight: "600" },
+        }),
+      },
+    },
+
+    {
+      name: "test",
+      options: {
+        filter: true,
+        setCellHeaderProps: (value) => ({
+          style: { fontSize: "15px", color: "black", fontWeight: "600" },
+        }),
+      },
+    },
+    {
+      name: "Action",
+
+      options: {
+        filter: true,
+        setCellHeaderProps: (value) => ({
+          style: {
+            width: "10px",
+            margin: "auto",
+            fontSize: "15px",
+            color: "black",
+            fontWeight: "600",
+          },
+        }),
+      },
+    },
+  ];
+
+  const arreasTableData = [
+    {
+      InvoiceNo: "test",
+      Delayed: "test",
+      reason: "test",
+      test: "test",
+      test: "test",
+      Action: (
+        <div>
+          <Button
+            variant="contained"
+            color="primary"
+            size="small"
+            className="btn_pay"
+          >
+            Update
+          </Button>
+        </div>
+      ),
+    },
+  ];
+
+  return (
+    <div>
+      {/*Start Installment Model Update */}
+
+      <Modal
+        visible={arreasUpdate}
+        className="arreas_update_Model"
+        footer={null}
+        onCancel={() => {
+          setArreasUpdate(false);
+        }}
+      >
+        <div className="arreas_Model">
+          <div className="arreas_update_Model_Main">
+            <div className="arreas_update_Modell_Detail"></div>
+          </div>
+        </div>
+      </Modal>
+
+      {/*END Installment Model Update */}
+
+      <Grid container spacing={4}>
+        <Grid item xs={12}>
+          <MUIDataTable
+            title={<span className="title_Span"></span>}
+            className="arreas_Table"
+            data={arreasTableData}
+            columns={arreasTableColomns}
+            options={{
+              selectableRows: false,
+              customToolbarSelect: () => {},
+              filterType: "checkbox",
+              download: false,
+              print: false,
+              searchPlaceholder: "Search using any column names",
+              elevation: 4,
+              sort: true,
+              onRowClick: (rowData, rowMeta) => {
+                setCurrentIndx(rowMeta.rowIndex);
+              },
+              textLabels: {
+                body: {
+                  noMatch: isLoading ? (
+                    <Spin className="tblSpinner" size="large" spinning="true" />
+                  ) : (
+                    <img
+                      alt="Empty data"
+                      className="empty_data"
+                      src={require("../../../../assets/empty.png")}
+                    />
+                  ),
+                },
+              },
+            }}
+          />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }

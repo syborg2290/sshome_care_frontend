d69b0b7063d498646f7514b717d421f1c3ddeb45
keptoon <launcherstechinfo@gmail.com>
2020-12-24 00:28:53 +0530

    bug fixing 81

--- src/main/admin/pages/item/stock_manage/components/Selected_item_Model/Selected_Item.js
+++ src/main/admin/pages/item/stock_manage/components/Selected_item_Model/Selected_Item.js
@@ -119,8 +119,8 @@ export default function Selected_Item({ itemListProps, closeModel }) {
                       eachItem.item.amountPerInstallment &&
                     ob.data().downPayment === eachItem.item.downPayment &&
                     ob.data().discount === eachItem.item.discount &&
-                    ob.data().stock_type === selectedType &&
-                    ob.data().modelNo[0] === eachItem.item.modelNo[0]
+                    ob.data().stock_type === selectedType
+                  // ob.data().modelNo[0] === eachItem.item.modelNo[0]
                 )
               ) {
                 var newArray = allItems.docs.filter(
@@ -136,8 +136,8 @@ export default function Selected_Item({ itemListProps, closeModel }) {
                       eachItem.item.amountPerInstallment &&
                     ob.data().downPayment === eachItem.item.downPayment &&
                     ob.data().discount === eachItem.item.discount &&
-                    ob.data().stock_type === selectedType &&
-                    ob.data().modelNo[0] === eachItem.item.modelNo[0]
+                    ob.data().stock_type === selectedType
+                  // ob.data().modelNo[0] === eachItem.item.modelNo[0]
                 );
                 if (newArray) {
                   var modelNosList = [];
